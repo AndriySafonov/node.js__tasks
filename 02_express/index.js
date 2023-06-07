049971a9a2c096698c9785cc5b6c39966394f50f
AndriySafonov <andreygold18@gmail.com>
2023-06-07 10:11:19 +0300

    started working with Express

---
+++ 02_express/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/user/:username', (req, res) => {
+  let data = {
+    username: req.params.username,
+    hobbies: ['Basketball', 'Programming'],
+  };
+  res.render('user', data);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started: http://localhost:${PORT}`);
+});
