8ec4e6d0953c6dbdd917af9e6436ff17ad79ee4e
AndriySafonov <andreygold18@gmail.com>
2023-07-18 11:54:52 +0300

    finish

--- 02_express/index.js
+++ 02_express/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -19,6 +20,12 @@ app.get('/user/:username', (req, res) => {
     hobbies: ['Basketball', 'Programming'],
   };
   res.render('user', data);
+});
+
+app.post('/check-user', (req, res) => {
+  let username = req.body.username;
+  if (username == '') return res.redirect('/');
+  else return res.redirect('/user/' + username);
 });
 
 const PORT = 3000;
